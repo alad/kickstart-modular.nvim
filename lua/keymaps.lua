6648f55cd80b819aec3575f019cdd550083c557a
Abhi Lad <abhimanyulad@gmail.com>
2025-09-03 14:28:36 -0700

    map j/k to gj/gk

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -38,6 +38,10 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- Abhi: Make j/k move one screen line instead of text line
+vim.keymap.set('n', 'j', 'gj', { desc = 'Move one line down' })
+vim.keymap.set('n', 'k', 'gk', { desc = 'Move one line up' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
