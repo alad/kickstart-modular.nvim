c0a2bad2a3cf7f35c82208833bac3d4da2a14027
Abhi Lad <abhimanyulad@gmail.com>
2025-10-09 13:00:12 -0700

    blink - show hidden files in path completions

--- lua/kickstart/plugins/blink-cmp.lua
+++ lua/kickstart/plugins/blink-cmp.lua
@@ -79,6 +79,11 @@ return {
         default = { 'lsp', 'path', 'snippets', 'lazydev' },
         providers = {
           lazydev = { module = 'lazydev.integrations.blink', score_offset = 100 },
+          path = {
+            opts = {
+              show_hidden_files_by_default = true,
+            },
+          },
         },
       },
 
